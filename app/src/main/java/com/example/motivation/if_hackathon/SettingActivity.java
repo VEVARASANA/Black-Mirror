d78320c3459ed551b63621cd3416482af0a8a9f1
YouBin Lee <ehsyb@naver.com>
2018-11-10 22:37:22 +0900

    Setting3

--- app/src/main/java/com/example/motivation/if_hackathon/SettingActivity.java
+++ app/src/main/java/com/example/motivation/if_hackathon/SettingActivity.java
@@ -2,6 +2,9 @@ package com.example.motivation.if_hackathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
+        LinearLayout linearLayout = findViewById(R.id.linearLayout);
+        linearLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                return true;
+            }
+        });
     }
 }
