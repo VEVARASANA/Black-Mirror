95b4f47543a59d889e699dfb3e09931b32f44fe4
JaeSangHan <41419137+JaeSangHan@users.noreply.github.com>
2018-11-11 00:42:16 +0900

    제스처

--- app/src/main/java/com/example/motivation/if_hackathon/SetPassword.java
+++ app/src/main/java/com/example/motivation/if_hackathon/SetPassword.java
@@ -2,6 +2,9 @@ package com.example.motivation.if_hackathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class SetPassword extends AppCompatActivity {
 
@@ -9,5 +12,67 @@ public class SetPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_password);
+
+        View view = findViewById(R.id.view);
+
+
+
+        view.setOnTouchListener(new View.OnTouchListener() {
+
+            float priX;
+            float priY;
+
+            float nexX;
+            float nexY;
+
+            float curX;
+            float curY;
+
+            int[] array;
+            int i = 0;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent motionEvent) {
+                int action = motionEvent.getAction();
+
+                curX = motionEvent.getX();
+                curY = motionEvent.getY();
+
+                if(action == MotionEvent.ACTION_DOWN){
+                    priX = curX;
+                    priY = curY;
+                }else if(action == MotionEvent.ACTION_UP){
+                    nexX = curX;
+                    nexY = curY;
+
+                    float resX = nexX - priX;
+                    float resY = nexY - priY;
+
+                    if( (resY - resX) > 0 && (resY + resX) > 0){                 //up
+                        array[i] = 1;
+                    } else if( (resY - resX) > 0 && (resY + resX) < 0){          //left
+                        array[i] = 2;
+                    } else if( (resY - resX) < 0 && (resY + resX) < 0){          //down
+                        array[i] = 3;
+                    } else if( (resY - resX) < 0 && (resY + resX) > 0){          //right
+                        array[i] = 4;
+                    }
+
+                    if(i < 3){
+                        i++;
+                        Log.d("test", "k");
+                    }else{
+                        Log.d("array",array.toString());
+                        i = 0;
+                    }
+
+                }
+
+                return true;
+            }
+        });
+
     }
+
+
 }
