c876a43ccfd92bd8c73c59053256d554cba4b193
JaeSangHan <41419137+JaeSangHan@users.noreply.github.com>
2018-11-11 07:01:09 +0900

    hjhkh

--- app/src/main/java/com/example/motivation/if_hackathon/LockScreenActivity.java
+++ app/src/main/java/com/example/motivation/if_hackathon/LockScreenActivity.java
@@ -52,6 +52,9 @@ public class LockScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
         setContentView(R.layout.activity_lock_screen);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
