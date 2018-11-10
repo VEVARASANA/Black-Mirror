d415bfed64f88c5a01a4e2b05adc278bb263ebaa
JaeSangHan <41419137+JaeSangHan@users.noreply.github.com>
2018-11-10 19:12:54 +0900

    TEST

--- app/src/main/java/com/example/motivation/if_hackathon/ScreenReceiver.java
+++ app/src/main/java/com/example/motivation/if_hackathon/ScreenReceiver.java
@@ -3,8 +3,12 @@ package com.example.motivation.if_hackathon;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
 
 public class ScreenReceiver extends BroadcastReceiver {
+
+    boolean IsOnLockScreen = false;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -13,6 +17,11 @@ public class ScreenReceiver extends BroadcastReceiver {
             Intent i = new Intent(context, LockScreenActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(i);
+            IsOnLockScreen = true;
+        }
+
+        if(intent.getAction().equals(Intent.ACTION_MEDIA_BUTTON) && IsOnLockScreen==true){
+            Log.d("nedia button", "test");
         }
     }
 }
