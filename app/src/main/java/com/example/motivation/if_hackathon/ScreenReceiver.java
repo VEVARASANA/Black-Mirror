68f918995e4482f645b757b6f50eccbde09f8165
JaeSangHan <41419137+JaeSangHan@users.noreply.github.com>
2018-11-11 06:38:29 +0900

    ygjg

--- app/src/main/java/com/example/motivation/if_hackathon/ScreenReceiver.java
+++ app/src/main/java/com/example/motivation/if_hackathon/ScreenReceiver.java
@@ -14,6 +14,7 @@ public class ScreenReceiver extends BroadcastReceiver {
         if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
             Intent i = new Intent(context, LockScreenActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            i.addFlags(intent.FLAG_ACTIVITY_SINGLE_TOP);
             context.startActivity(i);
         }
     }
