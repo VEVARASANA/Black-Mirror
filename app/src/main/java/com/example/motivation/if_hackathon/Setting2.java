3d21be214b900307682d9448d8677a36e2259150
YouBin Lee <ehsyb@naver.com>
2018-11-10 21:45:40 +0900

    add Setting

---
+++ app/src/main/java/com/example/motivation/if_hackathon/Setting2.java
@@ -0,0 +1,14 @@
+package com.example.motivation.if_hackathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Setting2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setCont;entView(R.layout.activity_setting2);
+        android
+    }
+}
