3c26a3e1a91cb7d4cb6b6eba368b53e4f70aeb9c
JaeSangHan <41419137+JaeSangHan@users.noreply.github.com>
2018-11-10 17:37:33 +0900

    Add Class To make fuction

---
+++ app/src/main/java/com/example/motivation/if_hackathon/LockScreenActivity.java
@@ -0,0 +1,17 @@
+package com.example.motivation.if_hackathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class LockScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock_screen);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+    }
+}
---
+++ app/src/main/java/com/example/motivation/if_hackathon/ScreenReciver.java
@@ -0,0 +1,18 @@
+package com.example.motivation.if_hackathon;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ScreenReciver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+            Intent i = new Intent(context, LockScreenActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+    }
+}
