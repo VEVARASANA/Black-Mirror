6001e635a880c5bffd5abea12d5c8e1ac4aaea30
JaeSangHan <41419137+JaeSangHan@users.noreply.github.com>
2018-11-11 07:05:18 +0900

    dfdf

--- app/src/main/java/com/example/motivation/if_hackathon/LockScreenActivity.java
+++ app/src/main/java/com/example/motivation/if_hackathon/LockScreenActivity.java
@@ -21,6 +21,7 @@ import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -52,12 +53,12 @@ public class LockScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_lock_screen);
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                        | WindowManager.LayoutParams.FLAG_FULLSCREEN
+                        |WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         final int permissonCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
 
