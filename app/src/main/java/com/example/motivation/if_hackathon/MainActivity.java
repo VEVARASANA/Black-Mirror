0a30a1a66614d7cef2b01b9f955ee7494e6001db
ChoiHyeongu <a11dsd123@gmail.com>
2018-11-10 21:45:25 +0900

    mainactivity

--- app/src/main/java/com/example/motivation/if_hackathon/MainActivity.java
+++ app/src/main/java/com/example/motivation/if_hackathon/MainActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SwitchCompat;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     SwitchCompat serviceSwitch;
+    LinearLayout settingLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 Intent intent = new Intent(MainActivity.this, ScreenService.class);
                 startService(intent);
+            }
+        });
+
+        settingLayout = (LinearLayout) findViewById(R.id.main_settingLayout);
+        settingLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
