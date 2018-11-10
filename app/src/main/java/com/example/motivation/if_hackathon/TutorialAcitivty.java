93d77319134484abe082667f53b83e0d8ed122fb
ChoiHyeongu <a11dsd123@gmail.com>
2018-11-10 19:17:08 +0900

    MainActivty switch

--- app/src/main/java/com/example/motivation/if_hackathon/TutorialAcitivty.java
+++ app/src/main/java/com/example/motivation/if_hackathon/TutorialAcitivty.java
@@ -1,8 +1,8 @@
 package com.example.motivation.if_hackathon;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -117,8 +117,8 @@ public class TutorialAcitivty extends AppCompatActivity {
                 startServiceButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Intent intent = new Intent(TutorialAcitivty.this, ScreenService.class);
-                        startService(intent);
+                        Intent intent = new Intent(TutorialAcitivty.this, MainActivity.class);
+                        startActivity(intent);
                     }
                 });
                 break;
