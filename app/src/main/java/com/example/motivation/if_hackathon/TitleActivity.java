9b818bc4338b11b2467871ba89b9577f0de16ca1
ChoiHyeongu <a11dsd123@gmail.com>
2018-11-11 07:14:52 +0900

    Black

--- app/src/main/java/com/example/motivation/if_hackathon/TitleActivity.java
+++ app/src/main/java/com/example/motivation/if_hackathon/TitleActivity.java
@@ -14,8 +14,8 @@ import android.widget.Toast;
 public class TitleActivity extends AppCompatActivity {
 
     Button nextButton;
-    final int permissonCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
-    private static final int PERMISSIONS_REQUEST_READ_SMS = 100;
+//    final int permissonCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+//    private static final int PERMISSIONS_REQUEST_READ_SMS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,20 +31,20 @@ public class TitleActivity extends AppCompatActivity {
             }
         });
 
-        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED
-                || ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(TitleActivity.this, new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
-        }
-
-        if (permissonCheck == PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(getApplicationContext(), "SMS 수신권한 있음", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(getApplicationContext(), "SMS 수신권한 없음", Toast.LENGTH_SHORT).show();
-            ActivityCompat.requestPermissions(TitleActivity.this,
-                    new String[]{Manifest.permission.SEND_SMS},
-                    PERMISSIONS_REQUEST_READ_SMS);
-
-        }
+//        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED
+//                || ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+//
+//            ActivityCompat.requestPermissions(TitleActivity.this, new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+//        }
+//
+//        if (permissonCheck == PackageManager.PERMISSION_GRANTED) {
+//            Toast.makeText(getApplicationContext(), "SMS 수신권한 있음", Toast.LENGTH_SHORT).show();
+//        } else {
+//            Toast.makeText(getApplicationContext(), "SMS 수신권한 없음", Toast.LENGTH_SHORT).show();
+//            ActivityCompat.requestPermissions(TitleActivity.this,
+//                    new String[]{Manifest.permission.SEND_SMS},
+//                    PERMISSIONS_REQUEST_READ_SMS);
+//
+//        }
     }
 }
